04fc1daa7baf5df41d862a5e23a1ca7f28ba26e8
Filip Mareček <58835Marecek@student.sstebrno.cz>
2024-05-20 08:19:11 +0200

    gg

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ramm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Members(object sender, RoutedEventArgs e)
+        {
+            win3 windown3 = new win3();
+            windown3.Show();
+            this.Close();
+        }
+
+        private void Button_Alba(object sender, RoutedEventArgs e)
+        {
+            Window2 win2 = new Window2();
+            win2.Show();
+            this.Close();
+        }
+    }
+}
---
+++ win3.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ramm
+{
+    /// <summary>
+    /// Interakční logika pro win3.xaml
+    /// </summary>
+    public partial class win3 : Window
+    {
+        public win3()
+        {
+            InitializeComponent();
+
+        }
+
+        private void richard(object sender, MouseButtonEventArgs e)
+        {
+            if (popup.IsOpen)
+                popup.IsOpen = false;
+            else
+                popup.IsOpen = true;
+        }
+
+        private void flake(object sender, MouseButtonEventArgs e)
+        {
+            if (popup2.IsOpen)
+                popup2.IsOpen = false;
+            else
+                popup2.IsOpen = true;
+        }
+
+        private void paul(object sender, MouseButtonEventArgs e)
+        {
+            if (popup3.IsOpen)
+                popup3.IsOpen = false;
+            else
+                popup3.IsOpen = true;
+        }
+
+        private void lindemann(object sender, MouseButtonEventArgs e)
+        {
+            if (popup4.IsOpen)
+                popup4.IsOpen = false;
+            else
+                popup4.IsOpen = true;
+        }
+
+        private void schneider(object sender, MouseButtonEventArgs e)
+        {
+            if (popup5.IsOpen)
+                popup5.IsOpen = false;
+            else
+                popup5.IsOpen = true;
+        }
+
+        private void riedel(object sender, MouseButtonEventArgs e)
+        {
+            if (popup6.IsOpen)
+                popup6.IsOpen = false;
+            else
+                popup6.IsOpen = true;
+        }
+
+        private void Button_Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
